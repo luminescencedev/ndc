a1d4e78f36f8e89f2416fee2a35875ab9e1046d2
Arthur <arthur.garnier1090@gmail.com>
2026-06-05 19:43:18 +0200

    Refactor game initialization and update methods for Tower Defense

--- game.py
+++ game.py
@@ -5,33 +5,33 @@ import math
 WIDTH = 256
 HEIGHT = 256
 
-CELL = 16
-
-TREE_X = 128
-TREE_Y = 128
-
-PREP = 0
-FIGHT = 1
-
-
-def dist(a, b, c, d):
-    return math.sqrt((a - c) ** 2 + (b - d) ** 2)
-
 class Game:
   def __init__(self):
-    pyxel.init(WIDTH, HEIGHT, title="Root Blight")
-    pyxel.mouse(True)
-    
-    self.round = 0  
-    self.state = PREP
-
-    self.new_round()
+    pyxel.init(WIDTH, HEIGHT, title="Tower Defense")
     pyxel.run(self.update, self.draw)
+    pyxel.mouse(False)
+    
+  def update(self):
+    pass
+  
+  def draw(self):
+    
+    pyxel.cls(0)
+    pyxel.text(10, 10, "Hello, Pyxel!", 7)
 
-    def update(self):
-      if self.state == PREP:
-          self.update_prep()
-      else:
-          self.update_fight()
+    self.draw_cursor()
+    self.draw_menu()
+    
+  def draw_menu(self):
+    pyxel.rect(50, 50, 100, 30, 1)
+    pyxel.text(60, 60, "Start Game", 7)
+    
+  
+  def draw_cursor(self):
+    x, y = pyxel.mouse_x, pyxel.mouse_y
+    pyxel.line(x - 5, y, x + 5, y, 7)
+    pyxel.line(x, y - 5, x, y + 5, 7)
+
+    
     
 Game()
